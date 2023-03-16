82a2599bf331521041e76e867868d16fad8c3056
Anthony Cyr <acyr@egyde.ca>
2023-03-16 16:03:37 -0400

    organization onboarding untested

---
+++ main.tf
@@ -0,0 +1,95 @@
+locals {
+  org_id = "153899115474"
+  project_id = "ermeticproject-379819"
+  member = "service_account:ermetic-test-2@ermeticproject-379819.iam.gserviceaccount.com"
+  region = "us-east1"
+  
+  
+  roles_list = ["roles/appengine.appViewer",
+  "roles/artifactregistry.reader",
+  "roles/bigquery.metadataViewer",
+  "roles/cloudasset.viewer",
+  "roles/compute.networkViewer",
+  "roles/compute.viewer",
+  "roles/container.viewer",
+  "roles/orgpolicy.policyViewer",
+  "roles/iam.securityReviewer",
+  "roles/resourcemanager.organizationViewer"
+  ]
+
+  inclusion_list = ["activity","data_access","policy"]
+  
+  exclusion_list = ["*",
+  "^system:",
+  "@container-engine-robot.iam.gserviceaccount.com$",
+  "@security-center-api.iam.gserviceaccount.com$"
+  ]
+
+}
+
+provider "google" {
+  project     = local.project_id
+  region      = local.region
+}
+resource "google_organization_iam_member" "adding-role-sa" {
+  for_each = toset(local.roles_list)
+
+  org_id  = local.org_id
+  role    = each.value
+  member  = local.member
+}
+
+resource "google_pubsub_topic" "ermetic-topic" {
+  name = "ermetic-topic"
+
+  message_retention_duration = "259200s"
+}
+
+resource "google_logging_organization_sink" "ermetic-audit-log" {
+  name   = "ermetic-audit-log"
+  description = "Sink created for the Ermetic Solution"
+  org_id = local.org_id
+
+  # Can export to pubsub, cloud storage, or bigquery
+  destination = "pubsub.googleapis.com/projects/${local.project_id}/topics/${google_pubsub_topic.ermetic-topic.name}"
+  for_each = toset(local.inclusion_list)
+  filter = "LOG_ID(cloudaudit.googleapis.com/${each.value}) OR"
+}
+
+resource "google_logging_organization_exclusion" "sink-exclusion" {
+  for_each = toset(local.exclusion_list)
+  name = "exclude-k8s-logs"
+  org_id = local.org_id
+
+  description = "Exclude kubernetes logs"
+
+  # Exclude all DEBUG or lower severity messages relating to instances
+  filter = "protoPayload.authenticationInfo.principalEmail=${each.value} OR"
+}
+
+resource "google_pubsub_subscription" "ermtetic-topic-sub" {
+  name  = "ermetic-topic-sub"
+  topic = google_pubsub_topic.ermetic-topic.name
+
+
+
+  #3 jours
+  message_retention_duration = "259200s"
+
+
+  ack_deadline_seconds = 10
+  #never expired
+  expiration_policy {
+    ttl = ""
+  }
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+
+  enable_message_ordering    = false
+}
+resource "google_pubsub_subscription_iam_member" "pubsub-subsciption-role" {
+  subscription = google_pubsub_subscription.ermtetic-topic-sub.name
+  role         = "roles/pubsub.subscriber"
+  member       = local.member
+}
